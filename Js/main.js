39213a62b0fc9e2305b0e32b5a2e0c025214bbdb
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-01 14:20:03 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,5 @@
+const numKm = parseInt(prompt("Qaunti chilometri deve percorrere?"));
+const userAge = parseInt(prompt("Qaunti anni ha?"));
+const kmPrice = 0.21;
+const minorsDiscount = 20;
+const over65Discount = 40;
