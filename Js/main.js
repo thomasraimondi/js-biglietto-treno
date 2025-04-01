69cea8a5a7aed139fd00236a41c6d0b6766af78d
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-01 14:20:40 +0200

    get user data and print in console

--- Js/main.js
+++ Js/main.js
@@ -3,3 +3,8 @@ const userAge = parseInt(prompt("Qaunti anni ha?"));
 const kmPrice = 0.21;
 const minorsDiscount = 20;
 const over65Discount = 40;
+
+console.log("Numero km: ", numKm);
+console.log("User Age: ", userAge);
+
+let messageOutput;
