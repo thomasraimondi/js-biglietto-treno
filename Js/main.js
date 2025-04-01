0342d6f1dfc2886bebdf151b7ae75a5b922a5988
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-01 14:54:13 +0200

    add colculate price minors discount

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,9 @@
-const numKm = parseInt(prompt("Qaunti chilometri deve percorrere?"));
-const userAge = parseInt(prompt("Qaunti anni ha?"));
+const numKm = parseInt(
+  prompt("Quanti chilometri deve percorrere?\n(inserire un valore numerico)")
+);
+const userAge = parseInt(
+  prompt("Qaunti anni ha?\n(inserire un valore numerico)")
+);
 const kmPrice = 0.21;
 const minorsDiscount = 20;
 const over65Discount = 40;
@@ -8,8 +12,28 @@ console.log("Numero km: ", numKm);
 console.log("User Age: ", userAge);
 
 let messageOutput;
+
 if (!isNaN(numKm) && !isNaN(userAge)) {
-  console.log("Costo bisoglietto: ", ticketPrice);
+  let humanPrice;
+  let discountedPrice;
+  let discount;
+  const ticketPrice = numKm * kmPrice;
+
+  if (userAge < 18) {
+    discount = (ticketPrice * minorsDiscount) / 100;
+    discountedPrice = ticketPrice - discount;
+    humanPrice = "€ " + discountedPrice.toFixed(2);
+    messageOutput = `Il costo del suo biglietto è di ${humanPrice}.\n\nApplicato sconto minorrenni del ${minorsDiscount}%`;
+
+    console.log("ticketPrice", ticketPrice);
+    console.log("discount", discount);
+    console.log("discountedPrice", discountedPrice);
+    console.log("humanPrice", humanPrice);
+    console.log(messageOutput);
+  } else {
+  }
 } else {
   console.log("valori non corretti");
 }
+
+alert(messageOutput);
